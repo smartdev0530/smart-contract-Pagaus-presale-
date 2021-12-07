c3fbbc2103d66ecc6061048b2da4f3457c4d55f5
dram <smdev00001@gmail.com>
2021-12-07 16:26:05 +0900

    change presale(1).sol

--- presale(1).sol
+++ presale(1).sol
@@ -1,126 +1,165 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 
-// SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.1;
+contract Presale_Swap is AccessControl, Pausable {
+    using SafeMath for uint256;
+    address _tokenArgh;
+    AggregatorV3Interface internal _priceFeed;
+    uint256 _tokenPrice = 0;
+    mapping(address => bool) public _hasPurchased;
+    address[] public _buyers;
+    uint256 _maxBNB_User = 4e18;
+    uint256 _minBNB_User = 1e17;
 
-import "./pegasus.sol";
+    /**
+     * Token Price in USD
+     * Decimals: 8
+     */
 
-interface AggregatorV3Interface {
+    /**
+     * Network: BSC Mainnet
+     * Aggregator: BNB/USD
+     * Address: 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
+     */
 
-  function decimals() external view returns (uint8);
-  function description() external view returns (string memory);
-  function version() external view returns (uint256);
-
-  // getRoundData and latestRoundData should both raise "No data present"
-  // if they do not have data to report, instead of returning unset values
-  // which could be misinterpreted as actual reported values.
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-}
-
-contract ICO {
-    using SafeMath for uint;
-    
-    Pegasus public token;
-    address public owner;
-    address public ceoAddress = 0xAF41Ed0573b65C43C78Afdee17BCae7f07e7A4C3;
-
-    uint availableToken;
-    uint MinPurchase=10000000;
-    uint MaxPurchase=400000000;
-
-    uint timeLimit1 = 1639132200; // unix timestamp in 2021.12.10 10:30 (UTC)
-    uint timeLimit2 = 1640428200; // unix timestamp in 2021.12.25 10:30 (UTC)
-    uint timeLimit3 = 1641810600; // unix timestamp in 2022.1.10 10:30 (UTC)
-
-    mapping (address => uint256) public buyerList;
-    mapping (address => uint256) public leftAmountList;
-
-    AggregatorV3Interface internal pricefeed;
-    
-    constructor (Pegasus _token){
-        token=_token;
-        owner=msg.sender;
-        // pricefeed = AggregatorV3Interface(0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526);
-        pricefeed = AggregatorV3Interface(0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE);
+    /**
+     * Network: BSC Testnet
+     * Aggregator: BNB/USD
+     * Address: 0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
+     */
+    constructor(address token) {
+        _tokenArgh = token;
+        _priceFeed = AggregatorV3Interface(
+            0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
+        );
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function buy(uint256 _tokenNum) external payable {
-        int256 data = getThePrice();
-        uint256 bnbPrice = uint256(data);
-        uint256 price = 30000000;
-        uint256 total = price.mul(_tokenNum);
-        total = total.mul(10**8);
-        total = total.div(bnbPrice);
-        require(buyerList[msg.sender] == 0, 'User can buy only 1 time');
-        require(total >= MinPurchase , 'Please Buy Pegasus more than 0.1BNB');
-        require(total <= MaxPurchase , 'Please Buy Pegasus less than 4BNB');
-        require(total <= msg.value , 'Please Send More Funds');
-        total = total * 10 ** 10;
-        payable(ceoAddress).transfer(total);
-        buyerList[msg.sender] = _tokenNum.mul(10**18);
-        leftAmountList[msg.sender] = 100;
-    }
-
-    function claim() external {
-      uint256 amount = buyerList[msg.sender];
-      require(amount > 0, 'Please buy token and claim');
-      if (block.timestamp <= timeLimit1 && leftAmountList[msg.sender] == 100) {
-        amount = amount.mul(33).div(100);
-        leftAmountList[msg.sender] = 77;
-        token.transfer(msg.sender, amount);
-      } else if (block.timestamp > timeLimit1 && block.timestamp <= timeLimit2 && leftAmountList[msg.sender] == 77) {
-        amount = amount.mul(33).div(100);
-        leftAmountList[msg.sender] = 34;
-        token.transfer(msg.sender, amount);
-      } else if (block.timestamp > timeLimit2 && block.timestamp <= timeLimit3 && leftAmountList[msg.sender] == 34) {
-        amount = amount.mul(34).div(100);
-        leftAmountList[msg.sender] = 0;
-        buyerList[msg.sender] = 0;
-        token.transfer(msg.sender, amount);
-      } 
-    }
-
-    function transferOwnership(address _owner) public{
-        require(msg.sender==owner);
-        owner=_owner;
-    }
-    
-    modifier onlyOwner(){
-        require(msg.sender==owner);
+    modifier onlyAdminRole() {
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Presale Swap: !admin"
+        );
         _;
     }
 
-    function getThePrice() public view returns (int) {
-        (
-            uint80 roundId, 
-            int256 answer,
-            uint256 startedAt,
-            uint256 updatedAt,
-            uint80 answeredInRound
-        ) = pricefeed.latestRoundData();
-        return answer;
+    function transferOwnership(address newOwner) public onlyAdminRole {
+        require(msg.sender != newOwner, "Presale Swap: !same address");
+        grantRole(DEFAULT_ADMIN_ROLE, newOwner);
+        revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
+
+    function swap() public payable whenNotPaused returns (bool) {
+        uint256 bnb_amount = msg.value;
+        (, int256 price, , , ) = _priceFeed.latestRoundData();
+
+        uint256 token_amount = (bnb_amount * uint256(price)) / _tokenPrice;
+
+        require(!_hasPurchased[msg.sender], "Presale Swap: Already purchased!");
+
+        require(
+            bnb_amount <= _maxBNB_User,
+            "Presale Swap: Exceed Max BNB Per User!"
+        );
+
+        require(
+            bnb_amount >= _minBNB_User,
+            "Presale Swap: Smaller than Min BNB!"
+        );
+
+        require(
+            ERC20(_tokenArgh).balanceOf(address(this)) >= token_amount,
+            "Presale Swap: Not enough Argh Token !"
+        );
+        _hasPurchased[msg.sender] = true;
+        _buyers.push(msg.sender);
+        bool res = ERC20(_tokenArgh).transfer(msg.sender, token_amount);
+        return res;
+    }
+
+    function sendToken(address wallet) public onlyAdminRole {
+        require(
+            ERC20(_tokenArgh).balanceOf(address(this)) > 0,
+            "Presale Swap: Token balance is zero !"
+        );
+        ERC20(_tokenArgh).transfer(
+            wallet,
+            ERC20(_tokenArgh).balanceOf(address(this))
+        );
+    }
+
+    function resetRound() public onlyAdminRole {
+        for (uint256 i = 0; i < _buyers.length; i++) {
+            _hasPurchased[_buyers[i]] = false;
+        }
+        delete _buyers;
+    }
+
+    function sendBNB(address wallet) public onlyAdminRole {
+        require(
+            address(this).balance > 0,
+            "Presale Swap: BNB balance is zero !"
+        );
+        address payable receiver = payable(wallet);
+        receiver.transfer(address(this).balance);
+    }
+
+    function setMaxBNB_User(uint256 amount) public onlyAdminRole {
+        _maxBNB_User = amount;
+    }
+
+    function getMaxBNB_User() public view returns (uint256) {
+        return _maxBNB_User;
+    }
+
+    function setMinBNB_User(uint256 amount) public onlyAdminRole {
+        _minBNB_User = amount;
+    }
+
+    function getMinBNB_User() public view returns (uint256) {
+        return _minBNB_User;
+    }
+    // function buringToken() pub{
+    //     burn(asdf)
+    // }
     
-    
+    function setTokenPrice(uint256 price) public onlyAdminRole {
+        _tokenPrice = price;
+    }
+
+    function getTokenPrice() public view returns (uint256) {
+        return _tokenPrice;
+    }
+
+    function getTokenAmount() public view returns (uint256) {
+        uint256 balance = ERC20(_tokenArgh).balanceOf(address(this));
+        return balance;
+    }
+
+    function getBNBAmount() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getBNBPrice() public view returns (uint256) {
+        (, int256 price, , , ) = _priceFeed.latestRoundData();
+        return uint256(price);
+    }
+
+    function pause() external onlyAdminRole {
+        super._pause();
+    }
+
+    function unpause() external onlyAdminRole {
+        super._unpause();
+    }
 }
+
+
